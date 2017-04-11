f6e7352c3a5cb9d12b8b69042bfda754ecda65d2
lux.r.ck <lux.r.ck@gmail.com>
2017-04-11 03:24:51 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import brainfuck
-from distutils.core import setup
+from setuptools import setup
 
 
-setup(name='python-brainfuck',
+setup(name='brainfuck',
       version=brainfuck.__version__,
       description='Python Brainfuck Interpretor',
       author='lux.r.ck',
       author_email='lux.r.ck@gmail.com',
       packages=['brainfuck'],
       include_package_data=True,
-      license="MIT License"
+      license="MIT License",
+      zip_safe=False,
      )
